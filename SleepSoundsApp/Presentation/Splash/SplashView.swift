f875424d66bfbd09b5c86992862f62cedf4a8b72
Ronado Vargas <ronaldovh99@gmail.com>
2024-11-19 10:00:33 -0500

    Se agrego .gitignore, cocopods, configuracion de project a iOS 15.0

---
+++ SleepSoundsApp/Presentation/Splash/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  SleepSoundsApp
+//
+//  Created by Ronaldo Andre on 19/11/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text("Splash View")
+    }
+}
+
+#Preview {
+    SplashView()
+}
