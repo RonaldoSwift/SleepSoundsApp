581d415a9b8bc18d06f4ffe50d0fd588a82b9add
Ronado Vargas <ronaldovh99@gmail.com>
2024-11-18 19:53:03 -0500

    Initial Commit

---
+++ SleepSoundsApp/SleepSoundsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepSoundsAppApp.swift
+//  SleepSoundsApp
+//
+//  Created by Ronaldo Andre on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SleepSoundsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
